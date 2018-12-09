2fc91053718f634f6ec71da92d13db0c4071640d
Juraj Veverka <juraj.veverka@pantheon.tech>
2018-12-09 09:19:40 +0100

    removed rpi sensehat example

--- sensehat-games/start-sensehat-demo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf sensehat-demo.log
-./sensehat-launcher.py &> sensehat-demo.log 
-
--- sensehat-games/stop-sensehat-demo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SENSE_PID=`ps -ef | grep launcher | grep python3 | awk '{ print $2 }'`
-kill ${SENSE_PID}
-
