1a367ce91fa0daf0dedb20fd535843c311915843
Juraj Veverka <juraj.veverka@pantheon.tech>
2019-04-10 15:07:34 +0200

    created ping-tool

--- windows-backup-server/ping-tool.bat
+++ windows-backup-server/ping-tool.bat
@@ -2,9 +2,9 @@ echo off
 
 set LOG_FILE=c:\backup\ping.log
 
-FOR /F "delims=" %%I IN ('powershell get-date -format "{yyyy-MM-dd_HH.mm.ss}"') DO SET DATESTR=%%I
 
 :LOOPSTART
+FOR /F "delims=" %%I IN ('powershell get-date -format "{yyyy-MM-dd_HH.mm.ss}"') DO SET DATESTR=%%I
 echo %DATESTR% >> %LOG_FILE%
 ping 8.8.8.8 -n 1 >> %LOG_FILE%
 timeout /t 10 > NUL
