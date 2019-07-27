c3670345fb7918caf9d6151cf0de25388b252543
Juraj Veverka <juraj.veverka@inter-net.sk>
2019-07-27 13:59:06 +0200

    removing microservices, mobved to separate repository

--- microservices/scripts/open-tunnel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ssh -L8001:127.0.0.1:8001 -i controlkey.pri juraj@192.168.56.101 kubectl proxy
-
--- microservices/scripts/shutdown-cluster.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ssh -i controlkey.pri root@192.168.56.103 poweroff
-ssh -i controlkey.pri root@192.168.56.102 poweroff
-ssh -i controlkey.pri root@192.168.56.101 poweroff
-
